891f54e9bed479c4123a192428767f8f8ef80849
Dan Orzechowski <orzechod@gmail.com>
2019-07-20 11:00:58 -0400

    make sure a buffer is still alive before attempting to mark it up

--- flymake-eslint.el
+++ flymake-eslint.el
@@ -124,6 +124,8 @@ Create linter process for SOURCE-BUFFER which invokes CALLBACK once linter is fi
          :sentinel (lambda (proc &rest ignored)
                      ;; do stuff upon child process termination
                      (when (and (eq 'exit (process-status proc))
+                                ;; make sure we're not using a deleted buffer
+                                (buffer-live-p source-buffer)
                                 ;; make sure we're using the latest lint process
                                 (with-current-buffer source-buffer (eq proc flymake-eslint--process)))
                        ;; read from eslint output then destroy temp buffer when done
